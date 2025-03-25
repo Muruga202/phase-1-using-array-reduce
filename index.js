d6d208b49ad5d8e483b9aca0fd8f37a73905ee3c
Maxwell Muruge <maxwell.muruga@student.moringaschool.com>
2025-03-25 20:32:49 +0300

    update

--- index.js
+++ index.js
@@ -1,3 +1,8 @@
 const batteryBatches = [4, 5, 3, 4, 4, 6, 5];
 
-// Code your solution here
+// Create a new variable called totalBatteries
+const totalBatteries = batteryBatches.reduce((accumulator, currentValue) => {
+  return accumulator + currentValue;
+}, 0);
+
+console.log(totalBatteries); // This will log the total number of batteries
